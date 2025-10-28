d02fac18aab6e084419e96aa1a9a26473cc0a0f9
leolucabernardi <leoluca.bernardi@outlook.com>
2025-10-28 09:27:40 +0100

    Outline for method implementations (only comments) [#19]

--- src/motors.cpp
+++ src/motors.cpp
@@ -1,6 +1,51 @@
 #include "../include/motors.hpp"
 
+/**
+* TODO: Method description
+*/
+GearMotor::GearMotor(int pin_1, int pin_2) {
+	// TODO: Method implementation
+}
 
-GearMotor::GearMotor() {
+/**
+* TODO: Method description
+*/
+int GearMotor::set_speed(int speed) {
+	// TODO: Method implementation
+}
+
+/**
+* TODO: Method description
+*/
+int GearMotor::forward(int speed) {
+	// TODO: Method implementation
+}
+
+/**
+* TODO: Method description
+*/
+int GearMotor::backward(int speed) {
+	// TODO: Method implementation
+}
+
+/**
+* TODO: Method description
+*/
+int GearMotor::coast() {
+	// TODO: Method implementation
+}
+
+/**
+* TODO: Method description
+*/
+int GearMotor::brake() {
+	// TODO: Method implementation
+}
+
+/**
+* TODO: Method description
+*/
+int GearMotor::stop() {
+	// TODO: Method implementation
 }
 
