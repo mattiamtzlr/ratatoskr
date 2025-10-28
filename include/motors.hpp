1735283af447f76c032a5968da7cb51ed8f976bc
leolucabernardi <leoluca.bernardi@outlook.com>
2025-10-28 18:06:12 +0100

    Include encoder in motor class [#19]

--- include/motors.hpp
+++ include/motors.hpp
@@ -10,7 +10,7 @@ enum DrivingMode {
 
 class GearMotor {
 public:
-	GearMotor(int pin_1, int pin_2);
+	GearMotor(int pin_1, int pin_2, int encoder_pin_1, int encoder_pin_2, int max_pwm);
 	int set_speed(int speed);
 	int forward(int speed);
 	int backward(int speed);
@@ -18,8 +18,11 @@ public:
 	int brake();
 	int stop();
 private:
-	int pin_1;
-	int pin_2;
-	int max_pwm;
+	const int PIN_1;
+	const int PIN_2;
+	const int ENCODER_PIN_1;
+	const int ENCODER_PIN_2;
+	const int MAX_PWM;
+	int rpm;
 	DrivingMode mode;
 };
--- src/motors.cpp
+++ src/motors.cpp
@@ -3,7 +3,7 @@
 /**
 * TODO: Method description
 */
-GearMotor::GearMotor(int pin_1, int pin_2) {
+GearMotor::GearMotor(int pin_1, int pin_2, int encoder_pin_1, int encoder_pin_2, int max_pwm) : PIN_1(pin_1), PIN_2(pin_2), ENCODER_PIN_1(encoder_pin_1), ENCODER_PIN_2(encoder_pin_2), MAX_PWM(max_pwm) {
 	// TODO: Method implementation
 }
 
