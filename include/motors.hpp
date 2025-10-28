b4248439425806450ed8b670a1cfa91e8af74642
Anthony Tamberg <anthony.tamberg@epfl.ch>
2025-10-28 19:05:48 +0100

    Prototype set_speed, forward, and backward methods [#19]
    This is going to need a lot more work.

--- include/motors.hpp
+++ include/motors.hpp
@@ -12,17 +12,25 @@ class GearMotor {
 public:
 	GearMotor(int pin_1, int pin_2, int encoder_pin_1, int encoder_pin_2, int max_pwm);
 	int set_speed(int speed);
+
 	int forward(int speed);
 	int backward(int speed);
 	int coast();
 	int brake();
 	int stop();
+	int get_rpm();
 private:
-	const int PIN_1;
-	const int PIN_2;
+	static constexpr int HIGH = 1;
+	static constexpr int LOW = 0;
+	static constexpr int MAX_PWM = 255; // TODO: Change this before running on real hardware!
+	static constexpr int MIN_PWM = 40; // TODO: Change this before running on real hardware!
+	static constexpr int MAX_RPM = 500; // TODO: Change this before running on real hardware!
+	static constexpr int MIN_RPM = 0; // TODO: Change this before running on real hardware!
+
+	const int IN1;
+	const int IN2;
 	const int ENCODER_PIN_1;
 	const int ENCODER_PIN_2;
-	const int MAX_PWM;
-	int rpm;
-	DrivingMode mode;
+	int m_rpm;
+	DrivingMode m_mode;
 };
--- src/motors.cpp
+++ src/motors.cpp
@@ -1,31 +1,58 @@
 #include "../include/motors.hpp"
+#include <Arduino.h>
+#include <cstdlib>
 
 /**
 * TODO: Method description
 */
-GearMotor::GearMotor(int pin_1, int pin_2, int encoder_pin_1, int encoder_pin_2, int max_pwm) : PIN_1(pin_1), PIN_2(pin_2), ENCODER_PIN_1(encoder_pin_1), ENCODER_PIN_2(encoder_pin_2), MAX_PWM(max_pwm) {
-	// TODO: Method implementation
+GearMotor::GearMotor(int in1, int in2, int encoder_pin_1, int encoder_pin_2, int max_pwm) : IN1(in1), IN2(in2), ENCODER_PIN_1(encoder_pin_1), ENCODER_PIN_2(encoder_pin_2) {
+	pinMode(IN1, OUTPUT);
+	pinMode(IN2, OUTPUT);
+
+	pinMode(ENCODER_PIN_1, INPUT_PULLUP);
+	pinMode(ENCODER_PIN_2, INPUT_PULLUP);
+	// TODO: finish this? i.e., handle interrupts, etc
+}
+
+int GearMotor::get_rpm() {
+	return m_rpm;
 }
 
 /**
 * TODO: Method description
 */
-int GearMotor::set_speed(int speed) {
-	// TODO: Method implementation
+int GearMotor::set_speed(int rpm) {
+	float rpm_perc = (float)(rpm - MIN_RPM) / MAX_RPM;
+	int pwm = (int)(MIN_PWM + (MAX_PWM - MIN_PWM) * rpm_perc);
+	switch(m_mode) {
+		case FORWARD:
+			analogWrite(IN2, pwm);
+			break;
+		case BACKWARD:
+			analogWrite(IN1, pwm);
+			break;
+		default:
+			break;
+	}
+	// TODO: Fix this because this is probably not right...
 }
 
 /**
 * TODO: Method description
 */
 int GearMotor::forward(int speed) {
-	// TODO: Method implementation
+	digitalWrite(IN1, LOW);
+	m_mode = FORWARD;
+	set_speed(speed);
 }
 
 /**
 * TODO: Method description
 */
 int GearMotor::backward(int speed) {
-	// TODO: Method implementation
+	digitalWrite(IN2, LOW);
+	m_mode = BACKWARD;
+	set_speed(speed);
 }
 
 /**
@@ -46,6 +73,7 @@ int GearMotor::brake() {
 * TODO: Method description
 */
 int GearMotor::stop() {
-	// TODO: Method implementation
+	digitalWrite(IN1, LOW);
+	digitalWrite(IN2, LOW);
 }
 
