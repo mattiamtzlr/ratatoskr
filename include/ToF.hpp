f71d174e8e7829835ee4032de4ded2d50711b2ef
Ali Gorgani <aligorgani2006@gmail.com>
2025-10-30 14:48:01 +0100

    [#17] Remove String and replace with enum
    We also remove overhead caused my m_initialized as it seems redundant.
    If it is set true in constructor, and not changed anywhere else, the
    check to see if it is initialized is to simply see if the object exists.

--- include/ToF.hpp
+++ include/ToF.hpp
@@ -1,11 +1,20 @@
 #include <Adafruit_VL53L1X.h>
-#include <Arduino.h>
 #include <Wire.h>
+
+  // Valid timing budgets: 15, 20, 33, 50, 100, 200 and 500ms! (from lib)
+#define TIMING_BUDGET 50
+
+enum SensorPosition {
+    LEFT        = 0,  // side-left
+    FRONT_LEFT  = 1,  // top-left
+    FRONT_RIGHT = 2,  // top-right
+    RIGHT       = 3   // side-right
+};
 
 class ToFVL53L1X {
    public:
     // Constructor performs initialization
-    ToFVL53L1X(String label, TwoWire *wire = &Wire, uint8_t i2c_address = 0x29);
+    ToFVL53L1X(SensorPosition position, TwoWire *wire = &Wire, uint8_t i2c_address = 0x29);
 
     // Start continuous ranging
     void start();
@@ -14,7 +23,6 @@ class ToFVL53L1X {
     uint16_t read();
 
    private:
-    String m_label;
+    SensorPosition m_position;
     Adafruit_VL53L1X m_sensor;
-    bool m_initialized;
 };
--- src/ToF.cpp
+++ src/ToF.cpp
@@ -1,32 +1,32 @@
 #include "ToF.hpp"
 
-ToFVL53L1X::ToFVL53L1X(String label, TwoWire *wire, uint8_t i2c_address)
-    : m_label(label), m_initialized(false) {
+ToFVL53L1X::ToFVL53L1X(SensorPosition position, TwoWire *wire, uint8_t i2c_address)
+    : m_position(position) {
     if (m_sensor.begin(i2c_address, wire)) {
-        m_sensor.setTimingBudget(50);  // 50 ms per measurement
-        m_initialized = true;
+        m_sensor.setTimingBudget(TIMING_BUDGET);  // 50 ms per measurement
     } else {
-        Serial.print("Failed to initialize VL53L1X: ");
-        Serial.println(m_label);
+        throw -1;
     }
 }
 
 void ToFVL53L1X::start() {
-    if (m_initialized) {
-        m_sensor.startRanging();
-    } else {
-        Serial.print("Cannot start VL53L1X (not initialized): ");
-        Serial.println(m_label);
-    }
+    m_sensor.startRanging();
 }
 
 uint16_t ToFVL53L1X::read() {
-    // I could still do the struct stuff but maybe its
-    // just easier to do this?
-    if (m_initialized && m_sensor.dataReady()) {
+    if (m_sensor.dataReady()) {
         uint16_t distance = m_sensor.distance();
         m_sensor.clearInterrupt();
         return distance;
     }
-    return 0;  // return 0 if not ready or not initialized
+    return 0;  // return 0 if not ready (same output in library)
+    /*
+        Good check we could keep (from the library example file)
+        if (distance == -1) {
+            // something went wrong!
+            Serial.print(F("Couldn't get distance: "));
+            Serial.println(vl53.vl_status);
+            return;
+        }
+    */
 }
