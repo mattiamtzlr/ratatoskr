122e8968be14c3e1bb69de908386857bd68737dd
Anthony Tamberg <anthony.tamberg@epfl.ch>
2025-11-03 18:40:13 +0100

    Complete basic mouse interface and maze class [#36]

--- include/maze.hpp
+++ include/maze.hpp
@@ -1,4 +1,20 @@
 #pragma once
 
+#include <cstddef>
+#include "util.hpp"
+
+constexpr int MAZE_WIDTH  = 8;
+constexpr int MAZE_HEIGHT = 8;
+
 class Maze {
+  private:
+    int m_wall_storage[MAZE_HEIGHT][MAZE_WIDTH] = {};
+    int m_distances[MAZE_HEIGHT][MAZE_WIDTH]    = {};
+
+  public:
+    void set_wall(size_t x, size_t y, Direction d);
+    void clear_wall(size_t x, size_t y, Direction d);
+    int get_walls_at(size_t x, size_t y);
+    int maze_width();
+    int maze_height();
 };
---
+++ src/mouse.cpp
@@ -0,0 +1,24 @@
+#include "mouse.hpp"
+#include "maze.hpp"
+
+Mouse::Mouse(Maze &maze) : m_maze(maze) {}
+
+void Mouse::turnLeft() { turn(90); }
+
+void Mouse::turnRight() { turn(-90); }
+
+void Mouse::setWall(int x, int y, char direction) {
+    m_maze.set_wall(x, y, direction_to_cardinal(direction));
+}
+
+void Mouse::clearWall(int x, int y, char direction) {
+    m_maze.clear_wall(x, y, direction_to_cardinal(direction));
+}
+
+int Mouse::mazeHeight() {
+    return m_maze.maze_height();
+}
+
+int Mouse::mazeWidth() {
+    return m_maze.maze_width();
+}
