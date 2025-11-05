a914b075fab6ffc2d9e33774a877f21f750a2a70
Ali Gorgani <aligorgani2006@gmail.com>
2025-11-05 18:52:34 +0100

    [#17] Rely on adafruit genius

--- src/ToF.cpp
+++ src/ToF.cpp
@@ -1,32 +1,32 @@
-#include "ToF.hpp"
+// #include "ToF.hpp"
 
-ToF::ToF(SensorPosition position, uint8_t shutdown_pin,
-                       uint8_t irq_pin, uint8_t i2c_address, TwoWire *wire = &Wire)
-    : position(position) {
-    m_sensor = Adafruit_VL53L1X(shutdown_pin, irq_pin);
-    if (m_sensor.begin(i2c_address, wire)) {
-        m_sensor.setTimingBudget(TIMING_BUDGET);  // 50 ms per measurement
-    } else {
-        throw -1;
-    }
-}
+// ToF::ToF(SensorPosition position, uint8_t shutdown_pin,
+//                        uint8_t irq_pin, uint8_t i2c_address, TwoWire *wire = &Wire)
+//     : position(position) {
+//     m_sensor = Adafruit_VL53L1X(shutdown_pin, irq_pin);
+//     if (m_sensor.begin(i2c_address, wire)) {
+//         m_sensor.setTimingBudget(TIMING_BUDGET);  // 50 ms per measurement
+//     } else {
+//         throw -1;
+//     }
+// }
 
-void ToF::start() { m_sensor.startRanging(); }
+// void ToF::start() { m_sensor.startRanging(); }
 
-uint16_t ToF::read() {
-    if (m_sensor.dataReady()) {
-        uint16_t distance = m_sensor.distance();
-        m_sensor.clearInterrupt();
-        return distance;
-    }
-    return 0;  // return 0 if not ready (same output in library)
-    /*
-        Good check we could keep (from the library example file)
-        if (distance == -1) {
-            // something went wrong!
-            Serial.print(F("Couldn't get distance: "));
-            Serial.println(vl53.vl_status);
-            return;
-        }
-    */
-}
+// uint16_t ToF::read() {
+//     if (m_sensor.dataReady()) {
+//         uint16_t distance = m_sensor.distance();
+//         m_sensor.clearInterrupt();
+//         return distance;
+//     }
+//     return 0;  // return 0 if not ready (same output in library)
+//     /*
+//         Good check we could keep (from the library example file)
+//         if (distance == -1) {
+//             // something went wrong!
+//             Serial.print(F("Couldn't get distance: "));
+//             Serial.println(vl53.vl_status);
+//             return;
+//         }
+//     */
+// }
--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,59 @@
-#include <Arduino.h>
+#include "Adafruit_VL53L1X.h"
 
-#include "ToF.hpp"
-
-ToF *sensor = NULL;
+Adafruit_VL53L1X vl53 = Adafruit_VL53L1X(0, 0); // rip xshut and irq
 
 void setup() {
-    Serial.begin(9600);
-    Wire.begin();
-    *sensor = ToF(LEFT, 34, 35, 0x29,  Wire);
-    delay(1000);
-    sensor->start();
-    Serial.println("Starting: ");
+  Serial.begin(115200);
+  while (!Serial) delay(10);
+
+  Serial.println(F("Adafruit VL53L1X sensor demo"));
+
+  Wire.begin();
+  if (! vl53.begin(0x29, &Wire)) {
+    Serial.print(F("Error on init of VL sensor: "));
+    Serial.println(vl53.vl_status);
+    while (1)       delay(10);
+  }
+  Serial.println(F("VL53L1X sensor OK!"));
+
+  Serial.print(F("Sensor ID: 0x"));
+  Serial.println(vl53.sensorID(), HEX);
+
+  if (! vl53.startRanging()) {
+    Serial.print(F("Couldn't start ranging: "));
+    Serial.println(vl53.vl_status);
+    while (1)       delay(10);
+  }
+  Serial.println(F("Ranging started"));
+
+  // Valid timing budgets: 15, 20, 33, 50, 100, 200 and 500ms!
+  vl53.setTimingBudget(50);
+  Serial.print(F("Timing budget (ms): "));
+  Serial.println(vl53.getTimingBudget());
+
+  /*
+  vl.VL53L1X_SetDistanceThreshold(100, 300, 3, 1);
+  vl.VL53L1X_SetInterruptPolarity(0);
+  */
 }
 
 void loop() {
-    uint16_t reading = sensor->read();
-    Serial.println(reading);
-    delay(500);
+  int16_t distance;
+
+  if (vl53.dataReady()) {
+    // new measurement for the taking!
+    distance = vl53.distance();
+    if (distance == -1) {
+      // something went wrong!
+      Serial.print(F("Couldn't get distance: "));
+      Serial.println(vl53.vl_status);
+      return;
+    }
+    Serial.print(F("Distance: "));
+    Serial.print(distance);
+    Serial.println(" mm");
+
+    // data is read out, time for another reading!
+    vl53.clearInterrupt();
+  }
 }
