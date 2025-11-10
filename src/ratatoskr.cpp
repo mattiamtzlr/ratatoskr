f6e07204ca94bc77c2944531ac1a036817162849
Ali Gorgani <aligorgani2006@gmail.com>
2025-11-10 18:29:04 +0100

    [#24] Add TODOs and fix constructor of Ratatoskr

--- src/ratatoskr.cpp
+++ src/ratatoskr.cpp
@@ -3,7 +3,8 @@
 Ratatoskr::Ratatoskr(Maze &maze, GearMotor &motor_left, GearMotor &motor_right,
                      ToF &tof_left, ToF &tof_front_left, ToF &tof_front_right,
                      ToF &tof_right, MPU6050 &gyro, LEDMatrix &screen)
-    : m_motor_left(motor_left),
+    : Mouse(maze),
+      m_motor_left(motor_left),
       m_motor_right(motor_right),
       m_tof_left(tof_left),
       m_tof_front_left(tof_front_left),
@@ -21,6 +22,7 @@ Ratatoskr::Ratatoskr(Maze &maze, GearMotor &motor_left, GearMotor &motor_right,
  * driven ccw, idem for cw
  */
 void Ratatoskr::turn(int angle) {
+    // TODO: use gyro to close the loop; for now, differential spin.
     float time_to_turn = PERIOD / (360 * angle);  // relation using angle
     if (angle > 0) {  // move right wheel forward, left wheel back
         m_motor_left.spin_cw(TURN_PWM);
@@ -29,6 +31,7 @@ void Ratatoskr::turn(int angle) {
         m_motor_left.spin_ccw(TURN_PWM);
         m_motor_right.spin_ccw(TURN_PWM);
     }
+    // timing/gyro stop goes here
     delay(time_to_turn);
     stop();
 }
@@ -37,11 +40,13 @@ void Ratatoskr::turn(int angle) {
  * move @distance [one cell (16 cm)] forwards
  */
 void Ratatoskr::moveForward(int distance) {
+    // TODO: PID on encoders + wall following; for now, spin both forward briefly.
     Mouse::moveForward(distance);  // Don't remove this, this updates the
                                    // position of the mouse in the maze!
     float time_forward = distance * TIME_PER_CELL;
     m_motor_left.spin_ccw(FORWARD_PWM);
     m_motor_right.spin_cw(FORWARD_PWM);
+    // timing/odometry goes here
     delay(time_forward);
     stop();
 }
@@ -61,16 +66,11 @@ void Ratatoskr::stop() {
 bool Ratatoskr::wallFront() {
     const uint16_t d_fl = m_tof_front_left.read();
     const uint16_t d_fr = m_tof_front_right.read();
-
-    const bool fl_valid = (d_fl > 0);
-    const bool fr_valid = (d_fr > 0);
-
-    const bool fl_block = fl_valid && (d_fl < FRONT_WALL_MM);
-    const bool fr_block = fr_valid && (d_fr < FRONT_WALL_MM);
-
-    // If only one is valid, use it; if both, OR; if none, assume no wall
-    return fl_block || fr_block;
+    const bool fl = (d_fl > 0) && (d_fl < FRONT_WALL_MM);
+    const bool fr = (d_fr > 0) && (d_fr < FRONT_WALL_MM);
+    return fl || fr;
 }
+
 
 /**
  * Right wall if right ToF < SIDE_WALL_MM (ignoring zeros).
