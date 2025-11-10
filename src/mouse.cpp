a03cca354ddc66d1d64f19b5ba225575b2adc2b4
Anthony Tamberg <anthony.tamberg@epfl.ch>
2025-11-10 14:56:01 +0100

    Change direction_to_cardinal to cardinalCharToDir
    This was missed when Leo changed the conversion function to a map

--- src/mouse.cpp
+++ src/mouse.cpp
@@ -8,11 +8,11 @@ void Mouse::turnLeft() { turn(90); }
 void Mouse::turnRight() { turn(-90); }
 
 void Mouse::setWall(int x, int y, char direction) {
-    m_maze.set_wall(x, y, direction_to_cardinal(direction));
+    m_maze.set_wall(x, y, cardinalCharToDir[direction]);
 }
 
 void Mouse::clearWall(int x, int y, char direction) {
-    m_maze.clear_wall(x, y, direction_to_cardinal(direction));
+    m_maze.clear_wall(x, y, cardinalCharToDir[direction]);
 }
 
 int Mouse::mazeHeight() {
