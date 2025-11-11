933bb91ffc428f0b990478ab32513cafa9aaa23a
mattiamtzlr <mattia.metzler@epfl.ch>
2025-11-11 18:32:36 +0100

    Add function and required libraries to turn off WiFi and BT

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,3 @@
-
 #include "ratatoskr.hpp"
 
 ToF tof_left = ToF(LEFT, 0x29);
@@ -11,6 +10,8 @@ ToF tof_right = ToF(RIGHT, 0x26);
 #define MOTOR_L_IN1 2
 #define MOTOR_L_IN2 4
 #define ENC_L_OUT2 16
+#include "esp_bt.h"
+#include <WiFi.h>
 
 /* right motor, pins in order of ESP */
 #define ENC_R_OUT1 17
@@ -25,10 +26,20 @@ GearMotor motor_right(MOTOR_R_IN1, MOTOR_R_IN2, ENC_R_OUT1, ENC_R_OUT2, 50);
 Ratatoskr rat(motor_left, motor_right, tof_left, tof_left_front,
               tof_right_front, tof_right);
 
+
+void disableWireless() {
+  WiFi.mode(WIFI_OFF);
+  WiFi.setSleep(true);
+  if (btStarted()) btStop();
+  esp_bt_controller_disable();
+}
+
+
 void setup() {
     Serial.begin(115200);
 
     Wire.begin();
+    disableWireless();
 
     pinMode(MOTOR_L_IN1, OUTPUT);
     pinMode(MOTOR_L_IN2, OUTPUT);
