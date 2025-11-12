09ce5095e05e7349e03f3276d0d4cf667ec67d76
mattiamtzlr <mattia.metzler@epfl.ch>
2025-11-12 16:53:40 +0100

    Adjust motor pins to be coherent with the diagram

--- src/main.cpp
+++ src/main.cpp
@@ -8,17 +8,17 @@ ToF tof_left_front = ToF(FRONT_LEFT, 0x28);
 ToF tof_right_front = ToF(FRONT_RIGHT, 0x27);
 ToF tof_right = ToF(RIGHT, 0x26);
 
-/* left motor, pins in order of ESP */
-#define ENC_L_OUT1 17
-#define MOTOR_L_IN1 5
-#define MOTOR_L_IN2 18
-#define ENC_L_OUT2 19
+/* left motor, pins in order of ESP => motor A resp. 1 */
+#define MOTOR_L_IN1 12
+#define MOTOR_L_IN2 14
+#define ENC_L_OUT1 33
+#define ENC_L_OUT2 32
 
-/* right motor, pins in order of ESP */
-#define ENC_R_OUT1 15
-#define MOTOR_R_IN1 2
-#define MOTOR_R_IN2 4
-#define ENC_R_OUT2 16
+/* right motor, pins in order of ESP => motor B resp. 2 */
+#define MOTOR_R_IN1 27
+#define MOTOR_R_IN2 26
+#define ENC_R_OUT1 35
+#define ENC_R_OUT2 34
 
 Maze maze();
 GearMotor motor_left(MOTOR_L_IN1, MOTOR_L_IN2, ENC_L_OUT1, ENC_L_OUT2, 50);
@@ -56,7 +56,7 @@ void setup() {
 }
 
 void loop() {
-    rat.moveForward();
+    rat.moveForward(2);
     delay(2000);
     rat.turn(90);
     delay(2000);
