9502d62c7315ecd13bde008903535d0042893f50
leolucabernardi <leoluca.bernardi@outlook.com>
2025-11-24 10:08:46 +0100

    [#87] moving docstring from pid.cpp to pid.hpp

--- include/pid.hpp
+++ include/pid.hpp
@@ -3,7 +3,20 @@ class PID {
    public:
     PID(float k_p, float k_i, float k_d);
 
+    /**
+     * Update function for the PID.
+     *
+     * @param error and time since the last update.
+     * @return the corrected signal.
+     */
     float update(float time_step, float error);
+
+    /**
+     * Reset the PID.
+     *
+     * Will set the integral and the previous error to zero.
+     *
+     */
     void reset();
 
    private:
--- src/pid.cpp
+++ src/pid.cpp
@@ -6,12 +6,6 @@ PID::PID(float k_p, float k_i, float k_d) : k_p(k_p), k_i(k_i), k_d(k_d) {
     reset();
 }
 
-/**
- * Update function for the PID.
- *
- * @param error and time since the last update.
- * @return the corrected signal.
- */
 float PID::update(float time_step, float error) {
     integral += error * time_step;
     float derivative = (error - previous_error) / time_step;
@@ -20,12 +14,6 @@ float PID::update(float time_step, float error) {
     return k_p * error + k_i * integral + k_d * derivative;
 }
 
-/**
- * Reset the PID.
- *
- * Will set the integral and the previous error to zero.
- *
- */
 void PID::reset() {
     previous_error = 0;
     integral = 0;
