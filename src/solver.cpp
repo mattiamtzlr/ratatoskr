617117d04e885bd46319e5a4981096c7916229d9
leolucabernardi <leoluca.bernardi@outlook.com>
2025-11-25 18:09:29 +0100

    [#79] Simple version of run

--- src/solver.cpp
+++ src/solver.cpp
@@ -199,40 +199,8 @@ void Solver::run(std::vector<Position> solved) {
         if (next_position.x == m_mouse.getPosition().x &&
             next_position.y == m_mouse.getPosition().y)
             continue;
-        Position curr_position = m_mouse.getPosition();
-        std::vector<Position> diag_neighbors =
-            m_maze.valid_diag_neighbors(curr_position);
-        if (std::find(diag_neighbors.begin(), diag_neighbors.end(),
-                      next_position) != diag_neighbors.end()) {
-            if (next_position.x > curr_position.x &&
-                next_position.y > curr_position.y) {
-                std::cerr << "Wants to turn diag NE" << std::endl;
-                if (m_maze.exists_wall(curr_position, NORTH)) {
-                    std::cerr << "Wall to the NORTH, going EAST first"
-                              << std::endl;
-                    face(EAST);
-                    m_mouse.moveForwardHalf();
-                    face(NORTH_EAST);
-                } else {
-                    std::cerr << "No wall to the NORTH, going NORTH first"
-                              << std::endl;
-                    face(NORTH);
-                    m_mouse.moveForwardHalf();
-                    face(NORTH_EAST);
-                }
-            } else if (next_position.x > m_mouse.getPosition().x &&
-                       next_position.y < m_mouse.getPosition().y) {
-                face(SOUTH_EAST);
-            } else if (next_position.x < m_mouse.getPosition().x &&
-                       next_position.y < m_mouse.getPosition().y) {
-                face(SOUTH_WEST);
-            } else if (next_position.x < m_mouse.getPosition().x &&
-                       next_position.y > m_mouse.getPosition().y) {
-                face(NORTH_WEST);
-            }
-        } else {
-            face(dir_for_neighbor(next_position, m_mouse.getPosition()));
-            m_mouse.moveForward();
-        }
+
+        face(dir_for_neighbor(next_position, m_mouse.getPosition()));
+        m_mouse.moveForward();
     }
 }
